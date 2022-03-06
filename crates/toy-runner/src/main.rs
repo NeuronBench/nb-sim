da8c376bc6554571c20b8b9f9a481b60bb4ca30b
Greg Hale <imalsogreg@gmail.com>
2022-03-06 18:23:56 -0500

    Try to get the clock intervals working

--- crates/toy-runner/src/main.rs
+++ crates/toy-runner/src/main.rs
@@ -7,7 +7,7 @@ use crate::neuron::solution::INTERSTICIAL_FLUID;
 use reuron::constants::BODY_TEMPERATURE;
 use reuron::neuron;
 use reuron::neuron::segment::examples::giant_squid_axon;
-use reuron_commands::*;
+// use reuron_commands::*;
 
 #[derive(Debug)]
 struct State {
@@ -26,11 +26,11 @@ fn initial_state() -> State {
         time: Timestamp(0.0),
         steps: 0,
         batches: 0,
-        time_coefficient: 0.000001,
+        time_coefficient: 0.001,
         simulation_interval: Interval(10e-6),
         segment: giant_squid_axon(),
-        simulation_batch_size: 10,
-        display_rate: 2.0,
+        simulation_batch_size: 100,
+        display_rate: 10.0,
     }
 }
 
@@ -38,7 +38,7 @@ fn initial_state() -> State {
 async fn main() {
     let state = Arc::new(Mutex::new(initial_state()));
     let watcher_state = state.clone();
-    let watcher = tokio::task::spawn(watch(watcher_state));
+    let _watcher = tokio::task::spawn(watch(watcher_state));
     // let runner = tokio::task::spawn(async move { run(state) });
     run(state).await;
 
@@ -51,47 +51,53 @@ async fn watch(state: Arc<Mutex<State>>) {
     let mut now = SystemTime::now();
     let mut most_recent_display = now.clone();
     loop {
-        now = SystemTime::now();
-
         let wait_interval = {
             let state = state.lock().unwrap();
+            println!(
+                "step {:.10}, batch {:.5}, {:.2} ms: {:.1} mV",
+                state.steps,
+                state.batches,
+                state.time.0 * 1e3,
+                state.segment.membrane_potential.0
+            );
+
             let inter_display_interval = Duration::from_micros((1e6 / state.display_rate) as u64);
-            match now.duration_since(most_recent_display) {
-                Ok(interval) if interval > inter_display_interval => {
-                    most_recent_display = now;
-                    println!(
-                        "step {:.10}, batch {:.5}, {:.2} ms: {:.1} mV",
-                        state.steps,
-                        state.batches,
-                        state.time.0 * 1e3,
-                        state.segment.membrane_potential.0
-                    );
-                    None
-                }
-                Ok(interval) => Some(interval),
-                Err(_) => None,
-            }
+            let next_target_display_time = most_recent_display + inter_display_interval;
+            most_recent_display = next_target_display_time;
+
+            now = SystemTime::now();
+            next_target_display_time.duration_since(now)
         };
 
-        if let Some(interval) = wait_interval {
-            tokio::time::sleep(interval).await;
+        match wait_interval {
+            Ok(interval) if interval > Duration::ZERO => {
+                tokio::time::sleep(interval).await;
+            }
+            Ok(i) => {
+                println!("warning: clock is behind by {:?}", i);
+            }
+            Err(e) => {
+                println!("warning: clock monotonicity error: {:?}", e);
+            }
         };
     }
 }
 
 async fn run(state: Arc<Mutex<State>>) {
     let mut now = SystemTime::now();
+    let mut most_recent_simulation_wall_clock_time = now.clone();
 
     loop {
-        let delay_interval = {
+        let wait_interval = {
             let mut state = state.lock().unwrap();
 
             state.batches += 1;
             let inter_batch_wall_clock_interval = Duration::from_micros(
-                (1e6 * state.simulation_batch_size as f32
-                    * state.simulation_interval.0 as f32
-                    * state.time_coefficient) as u64,
+                (1e6 * state.simulation_batch_size as f32 * state.simulation_interval.0 as f32
+                    / state.time_coefficient) as u64,
             );
+            let next_target_simulation_time =
+                most_recent_simulation_wall_clock_time + inter_batch_wall_clock_interval;
 
             let batch_start_time = SystemTime::now();
 
@@ -103,14 +109,24 @@ async fn run(state: Arc<Mutex<State>>) {
                     .segment
                     .step(&BODY_TEMPERATURE, &INTERSTICIAL_FLUID, &interval);
             }
+            most_recent_simulation_wall_clock_time = next_target_simulation_time;
 
             now = SystemTime::now();
 
-            let delay_interval = now
-                .duration_since(batch_start_time)
-                .unwrap_or(inter_batch_wall_clock_interval);
-            delay_interval
+            next_target_simulation_time.duration_since(now)
         };
-        tokio::time::sleep(delay_interval).await;
+        dbg!(&wait_interval);
+
+        match wait_interval {
+            Ok(interval) if interval > Duration::ZERO => {
+                tokio::time::sleep(interval).await;
+            }
+            Ok(i) => {
+                println!("warning: clock is behind by {:?}", i);
+            }
+            Err(e) => {
+                println!("warning: clock monotonicity error: {:?}", e);
+            }
+        };
     }
 }
