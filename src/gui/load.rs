5941543b93c82d5082bce48ee3f294e4698e242d
Gregory Hale <imalsogreg@gmail.com>
2023-06-17 09:38:51 -0700

    Get ffg_source from url querystring when available

--- src/gui/load.rs
+++ src/gui/load.rs
@@ -3,12 +3,17 @@ use bevy_egui::{egui::{self, Ui}, EguiContexts};
 use bevy::tasks::{IoTaskPool, Task};
 use ehttp::{Request, Response, fetch};
 use crossbeam::channel::unbounded;
+use web_sys::{Location, window};
 
 use crate::neuron::ecs::Neuron;
 use crate::neuron::Junction;
 use crate::neuron::segment::ecs::Segment;
 use crate::stimulator::{Stimulation};
-use crate::integrations::grace::{GraceScene, GraceSceneSender, GraceSceneReceiver};
+use crate::integrations::grace::{
+    GraceScene,
+    GraceSceneSender,
+    GraceSceneReceiver
+};
 use crate::serialize;
 use crate::neuron::membrane::{MembraneMaterials};
 
@@ -18,13 +23,111 @@ pub struct IsLoading(pub bool);
 #[derive(Resource)]
 pub struct GraceSceneSource(pub String);
 
+impl FromWorld for GraceSceneSource {
+
+    #[cfg(target_arch = "wasm32")]
+    fn from_world(world: &mut World) -> Self {
+        GraceSceneSource(window_location_scene())
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    fn from_world(world: &mut World) -> Self {
+        GraceSceneSource("native-todo".to_string())
+    }
+
+}
+
+#[cfg(target_arch = "wasm32")]
+/// Parse an ffg expression from the browser's window.location.
+pub fn window_location_scene() -> String {
+    let search = window().expect("should have window").location().search();
+    match search {
+        Ok(s) => {
+            let s = s.clone().to_string();
+            let params = querystring::querify(&s[1..]);
+            match params.iter().find(|(k,v)| k.clone() == "scene") {
+                Some((_,v)) => { v.to_string() },
+                None => { "".to_string() },
+            }
+        },
+        Err(_) => {
+            "".to_string()
+        }
+    }
+}
+
 
 pub fn setup(app: &mut App) {
   app.insert_resource(IsLoading(false));
-  app.insert_resource(GraceSceneSource("https://raw.githubusercontent.com/reuron/reuron-lib/main/scene.ffg".to_string()));
+  // app.insert_resource(GraceSceneSource("https://raw.githubusercontent.com/reuron/reuron-lib/main/scene.ffg".to_string()));
+  app.init_resource::<GraceSceneSource>();
   let (tx, rx) = unbounded();
   app.insert_resource(GraceSceneSender(tx));
   app.insert_resource(GraceSceneReceiver(rx));
+  app.add_system(load_ffg_scene2);
+}
+
+pub fn load_ffg_scene2(
+    mut commands: Commands,
+    // mut ui: &mut Ui,
+    mut is_loading: ResMut<IsLoading>,
+    mut source: ResMut<GraceSceneSource>,
+    mut neurons: Query<(Entity, &Neuron)>,
+    mut segments: Query<(Entity, &Segment)>,
+    mut junctions: Query<(Entity, &Junction)>,
+    mut stimulations: Query<(Entity, &Stimulation)>,
+    grace_scene_sender: Res<GraceSceneSender>,
+
+) { }
+
+pub fn load_ffg_scene(
+    mut commands: &mut Commands,
+    mut is_loading: ResMut<IsLoading>,
+    mut source: ResMut<GraceSceneSource>,
+    mut neurons: Query<(Entity, &Neuron)>,
+    mut segments: Query<(Entity, &Segment)>,
+    mut junctions: Query<(Entity, &Junction)>,
+    mut stimulations: Query<(Entity, &Stimulation)>,
+    grace_scene_sender: Res<GraceSceneSender>,
+
+) {
+    for (entity, stimulation) in &mut stimulations {
+        commands.entity(entity).despawn();
+    }
+    for (entity, junction) in &mut junctions {
+        commands.entity(entity).despawn();
+    }
+    for (entity, segment) in &mut segments {
+        commands.entity(entity).despawn();
+    }
+    for (entity, neuron) in &mut neurons {
+        commands.entity(entity).despawn();
+    }
+    println!("Requesting from reuron.io: {}", source.0);
+    let request = Request::post("https://reuron.io/interpret", source.0.clone().into_bytes());
+    let sender = (*grace_scene_sender).clone();
+    fetch(request, move |response| {
+        match response {
+            Err(_) => {
+                eprintln!("fetch error");
+            },
+            Ok(r) => {
+                println!("response: {:?}", r);
+                match r.text().ok_or_else(|| {
+                    panic!("No response text!")
+                }).and_then(|n| serde_json::from_str::<serialize::Scene>(n)) {
+                    Ok(grace_scene) => {
+                        // TODO: Simplify all neurons.
+                        sender.0.send(GraceScene(grace_scene)).expect("Send should succeed");
+
+                    },
+                    Err(e) => {
+                        panic!("{:?}",e)
+                    },
+                }
+            },
+        }
+    })
 }
 
 pub fn run_grace_load_widget(
@@ -40,43 +143,7 @@ pub fn run_grace_load_widget(
 ) {
     let response = ui.add(egui::TextEdit::singleline(&mut source.0));
     if ui.button("Load").clicked() {
-        for (entity, stimulation) in &mut stimulations {
-            commands.entity(entity).despawn();
-        }
-        for (entity, junction) in &mut junctions {
-            commands.entity(entity).despawn();
-        }
-        for (entity, segment) in &mut segments {
-            commands.entity(entity).despawn();
-        }
-        for (entity, neuron) in &mut neurons {
-            commands.entity(entity).despawn();
-        }
-        println!("Requesting from reuron.io: {}", source.0);
-        let request = Request::post("https://reuron.io/interpret", source.0.clone().into_bytes());
-        let sender = (*grace_scene_sender).clone();
-        fetch(request, move |response| {
-            match response {
-                Err(_) => {
-                    eprintln!("fetch error");
-                },
-                Ok(r) => {
-                    println!("response: {:?}", r);
-                    match r.text().ok_or_else(|| {
-                        panic!("No response text!")
-                    }).and_then(|n| serde_json::from_str::<serialize::Scene>(n)) {
-                        Ok(grace_scene) => {
-                            // TODO: Simplify all neurons.
-                            sender.0.send(GraceScene(grace_scene)).expect("Send should succeed");
-
-                        },
-                        Err(e) => {
-                            panic!("{:?}",e)
-                        },
-                    }
-                },
-            }
-        })
+        load_ffg_scene(commands, is_loading, source, neurons, segments, junctions, stimulations, grace_scene_sender);
     }
 }
 
